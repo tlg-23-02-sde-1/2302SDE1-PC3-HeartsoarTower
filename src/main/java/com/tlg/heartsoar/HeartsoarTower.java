671a3819dd26afae2cb17df80cd5917bbf636a1b
Chance Jolley <chance.jolley@tlgcohort.com>
2023-05-18 10:36:29 -0700

    newGame method implemented into HeartsoarTower AB#148

--- src/main/java/com/tlg/heartsoar/HeartsoarTower.java
+++ src/main/java/com/tlg/heartsoar/HeartsoarTower.java
@@ -1,5 +1,24 @@
 package com.tlg.heartsoar;
 
+import java.util.Scanner;
+
 class HeartsoarTower {
 
+    void newGame() {
+        Scanner inputScanner = new Scanner(System.in);
+        String userInput;
+
+        System.out.println("Welcome to Heartsoar Tower!");
+        System.out.println("Please enter 'New Game' to start a new game:");
+        userInput = inputScanner.nextLine();
+        if ("New Game".equalsIgnoreCase(userInput)) {
+            System.out.println("Starting a new game...");
+            //playGame()
+        } else {
+            System.out.println("Invalid. Please enter 'New Game' to start the game.");
+        }
+
+        inputScanner.close();
+    }
 }
+
