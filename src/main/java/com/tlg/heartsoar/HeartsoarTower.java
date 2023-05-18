9cb5b75f9ca83afc398aa301913c0c1daa52b0b1
Chance Jolley <chance.jolley@tlgcohort.com>
2023-05-18 11:34:28 -0700

    quitGame method implemented into HeartsoarTower AB#149

--- src/main/java/com/tlg/heartsoar/HeartsoarTower.java
+++ src/main/java/com/tlg/heartsoar/HeartsoarTower.java
@@ -29,8 +29,24 @@ class HeartsoarTower {
         } else {
             System.out.println("Invalid. Please enter 'New Game' to start the game.");
         }
-
-        inputScanner.close();
     }
+
+    void quitGame() {
+        System.out.println("Would you like to quit the game? Y/N");
+        String userInput = scanner.nextLine();
+        while (!"Y".equalsIgnoreCase(userInput) && !"Yes".equalsIgnoreCase(userInput) &&
+                !"N".equalsIgnoreCase(userInput) && !"No".equalsIgnoreCase(userInput)) {
+            System.out.println("Invalid input. Please enter Y/Yes to confirm quitting, or N/No to continue playing.");
+            userInput = scanner.nextLine();
+        }
+        if ("Y".equalsIgnoreCase(userInput) || "Yes".equalsIgnoreCase(userInput)) {
+            System.out.println("Quitting the game. Goodbye!");
+            System.exit(0);
+        } else {
+            System.out.println("Returning to the start..");
+            newGame();
+        }
+    }
+
 }
 
