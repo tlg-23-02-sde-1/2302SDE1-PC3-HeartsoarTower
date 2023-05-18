3d2618f5f3e8c8963ca98662ba848111c5de8fdf
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-05-18 11:34:02 -0600

    stubbed nouns and verbs for textparser.  textparser returns proper nouns and verbs.  blocked by json input. AB#151

--- src/main/java/com/tlg/heartsoar/TextParser.java
+++ src/main/java/com/tlg/heartsoar/TextParser.java
@@ -1,5 +1,37 @@
 package com.tlg.heartsoar;
 
+import java.util.List;
+
 class TextParser {
+//    TODO: Make singleton
+    private final List<String> NOUNS;
+    private final List<String> VERBS;
+//    TODO: Read nouns and verbs from JSON file
+
+    TextParser(List<String> noun, List<String> verbs) {
+        NOUNS = noun;
+        VERBS = verbs;
+    }
+
+    String[] validCombo(String input){
+        String verb = null;
+        String noun = null;
+        // Split the input by spaces.  Check for a verb.  Check for a noun.
+        String[] words = input.split(" ");
+        for (String word : words) {
+            if (VERBS.contains(word)) {
+                verb = word;
+            }
+            if (NOUNS.contains(word)) {
+                noun = word;
+            }
+        }
+        if (verb == null || noun == null) {
+            return null;
+        }
+        return new String[]{verb, noun};
+    }
+
+
 
 }
