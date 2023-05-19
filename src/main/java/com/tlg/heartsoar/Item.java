93daa2a9b16ea250ae099ac4ada5f69cb10d0238
JoshuaERichardson <joshuarichardson1478@gmail.com>
2023-05-18 20:07:27 -0600

    pushing for uml purposes

--- src/main/java/com/tlg/heartsoar/Item.java
+++ src/main/java/com/tlg/heartsoar/Item.java
@@ -1,5 +1,12 @@
 package com.tlg.heartsoar;
 
 class Item {
-
+    private String name;
+    private String description;
+    private boolean isStorable;
+    public Item(String name, String description, boolean isStorable) {
+        this.name = name;
+        this.description = description;
+        this.isStorable = isStorable;
+    }
 }
--- src/main/java/com/tlg/heartsoar/Monster.java
+++ src/main/java/com/tlg/heartsoar/Monster.java
@@ -1,5 +1,41 @@
 package com.tlg.heartsoar;
 
-public class Monster {
+import java.util.List;
 
+public class Monster {
+    private String name;
+    private boolean isAlive = true;
+    private List<String> failures; // Single words that result in failure
+    private List<String> successes; // Noun + verb that result in success
+    private List<String> desc; // Description of the monster after each success action
+    private String sceneFailed; // Scene to go to if the player fails to kill the monster
+
+    public Monster(String name, List<String> failures, List<String> successes, List<String> desc, String sceneFailed) {
+        this.name = name;
+        this.failures = failures;
+        this.successes = successes;
+        this.desc = desc;
+        this.sceneFailed = sceneFailed;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public List<String> getFailures() {
+        return failures;
+    }
+
+    public List<String> getSuccesses() {
+        return successes;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
 }
