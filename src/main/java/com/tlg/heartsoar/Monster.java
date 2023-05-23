4352e5b6eb56451696e095727af3b1b2a20228ad
Chance Jolley <chancejolley@msn.com>
2023-05-23 13:56:59 -0700

    added dialogue function to Monster Class AB#172

--- src/main/java/com/tlg/heartsoar/Monster.java
+++ src/main/java/com/tlg/heartsoar/Monster.java
@@ -1,6 +1,7 @@
 package com.tlg.heartsoar;
 
 import java.util.List;
+import java.util.Random;
 
 public class Monster {
     private String name;
@@ -9,14 +10,18 @@ public class Monster {
     private List<String> successes; // Noun + verb that result in success
     private List<String> desc; // Description of the monster after each success action
     private String sceneFailed; // Scene to go to if the player fails to kill the monster
+    private List<String> dialogue;
 
-    public Monster(String name, List<String> successes, List<String> failures, List<String> desc, String sceneFailed) {
+
+
+    public Monster(String name, List<String> successes, List<String> failures, List<String> desc, String sceneFailed, List<String> dialogue) {
         this.name = name;
         this.successes = successes;
         this.failures = failures;
         this.desc = desc;
         this.sceneFailed = sceneFailed;
         this.isAlive = true;
+        this.dialogue = dialogue;
     }
 
 
@@ -26,6 +31,12 @@ public class Monster {
 
     public boolean isAlive() {
         return isAlive;
+    }
+
+    public String talk() {
+        Random random = new Random();
+        int dialogueIndex = random.nextInt(dialogue.size());  // Randomly select a dialogue
+        return dialogue.get(dialogueIndex);  // Return the selected dialogue
     }
 
     public List<String> getFailures() {
