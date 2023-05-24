fe56005c3a4ad141d2bbd971232206e49c86015a
Chance Jolley <chancejolley@msn.com>
2023-05-24 13:29:09 -0700

    Displaying title screen after rework of structure, and .JARing program for release.

--- src/main/java/com/tlg/view/TitleScreen.java
+++ src/main/java/com/tlg/view/TitleScreen.java
@@ -5,57 +5,28 @@ import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 
-
 public class TitleScreen {
 
     private static String titleScreen;
 
-    public static void displayTitleScreen() {
-//        Two threads -- one to display the title screen, the other to wait for the user to press Enter.
-        Thread thread1 = new Thread(() -> {
-            displayTitleScreen(titleScreen);
-//            Sleep for 5 seconds:
-            try {
-                Thread.sleep(5000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-        Thread thread2 = new Thread(() -> {
-//            Cancel thread1's sleep if we press a key:
-            Scanner scanner = new Scanner(System.in);
-            scanner.nextLine();
-            thread1.interrupt();
-        });
-//        thread1.start();
-//        thread2.start();
 
-//        System.out.println("\nPress Enter to continue...");
-//        Scanner scanner = new Scanner(System.in);
-//        scanner.nextLine();
+    public static void displayTitleScreen() {
+        displayTitleScreen(titleScreen);
+
+        System.out.println("\nPress Enter to continue:");
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
 
         clearScreen();
     }
 
     private static void displayTitleScreen(String asciiArt) {
-        System.out.print(asciiArt);
-    }
+       System.out.print(asciiArt);
+   }
 
     private static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
-//        Clear the console screen:
-//        try{
-//            final String os = System.getProperty("os.name");
-//            if (os.contains("Windows")) {
-//                Runtime.getRuntime().exec("cls");
-//            } else {
-//                Runtime.getRuntime().exec("clear");
-//            }
-//
-//        } catch (final Exception e) {
-//            e.printStackTrace();
-//        }
 
     }
 
